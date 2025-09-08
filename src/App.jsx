85116262e826e2b968661a2f478236ed266e008e
Nicollasn3 <nicollas.n3dsgn@gmail.com>
2025-09-08 10:52:40 -0300

    aaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar' 
 import Problemas from './components/Problemas'
 import Loading from './components/Loading'
