99823e2f4ad519d4d3890c88f80c0c2379a63982
Nicollasn3 <nicollas.n3dsgn@gmail.com>
2025-10-19 12:52:58 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          gsap: ['gsap', '@gsap/react']
+        }
+      }
+    }
+  },
+  base: './' // Importante para funcionar em subdiretórios
 })
